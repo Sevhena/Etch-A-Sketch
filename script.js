aaa3fb95a06a6eb8ec55f0ebd3d0bb7f1e0e0d12
Eldritch <83547364+Sevhena@users.noreply.github.com>
2023-07-12 12:40:28 -0400

    Add colour changing hover-effect

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ function createGrid(height, width) {
 
         const row = document.createElement('div');
         row.classList.toggle('row');
+
         for (let j = 0; j < width; j++) {
             const block = document.createElement('div');
             block.classList.toggle('block');
             row.appendChild(block);
         }
+
         grid.appendChild(row);
     }
 }
@@ -16,3 +18,12 @@ function createGrid(height, width) {
 const grid = document.querySelector("#grid");
 createGrid(16, 16);
 
+const blocks = document.querySelectorAll('.block');
+
+blocks.forEach(block => {
+
+    block.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'green';
+        console.log(event.target);
+    });
+});
